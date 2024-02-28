076c3cd018cc8125b8261d41803c7160db982602
BaeTab <bhwoo48@gmail.com>
2024-02-28 16:43:08 +0900

    - Password Agent init Commit

---
+++ PasswordAgent/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace PasswordAgent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 177);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "입력";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(227, 148);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "폴더 열기";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(292, 12);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "로그인 페이지 URL또는 임의의 이름을 입력해 주세요";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 111);
+            this.textBox3.MaxLength = 200;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(221, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "이름";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 66);
+            this.textBox2.MaxLength = 30;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(221, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 25);
+            this.textBox1.MaxLength = 30;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(2, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(315, 113);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "조회";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Enabled = false;
+            this.label7.Location = new System.Drawing.Point(82, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 12);
+            this.label7.TabIndex = 19;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(181, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "복사";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "복사";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Location = new System.Drawing.Point(75, 72);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.PasswordChar = '*';
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(104, 21);
+            this.textBox6.TabIndex = 0;
+            this.textBox6.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ID";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Location = new System.Drawing.Point(75, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(104, 21);
+            this.textBox5.TabIndex = 0;
+            this.textBox5.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Location = new System.Drawing.Point(75, 16);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(185, 21);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "이름";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(323, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 292);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(502, 314);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Don\'t Forget My Password";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PasswordAgent/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PasswordAgent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initControl();
+            loadUrlsToListBox();
+        }
+
+        private void initControl()
+        {
+            button1.Click += Button1_Click;
+            button3.Click += Button3_Click;
+            button4.Click += Button4_Click;
+            button6.Click += Button6_Click;
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+        }
+
+        public string filePath = "pwAgent.xml";
+
+
+        private void Button1_Click(object sender, EventArgs e) // 데이터 저장을 위한 버튼
+        {
+            string id = textBox1.Text;
+            string pw = textBox2.Text;
+            string url = textBox3.Text;
+
+            try
+            {
+                if (string.Empty == filePath || string.Empty == id || string.Empty == pw || string.Empty == url)
+                {
+                    MessageBox.Show("빈 값이 있습니다", "오류");
+                    return;
+                }
+                else
+                {
+                    if (!System.IO.File.Exists(filePath))
+                    {
+                        SavePwAgentToXml(id, pw, url, filePath);
+                        MessageBox.Show("저장 되었습니다", "알림");
+                    }
+                    else
+                    {
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(filePath);
+
+                        // 중복 체크
+                        XmlNodeList existingpwAgent = doc.SelectNodes($"//pwAgent[url='{url}']");
+                        if (existingpwAgent.Count > 0)
+                        {
+                            MessageBox.Show("해당 URL 정보는 저장되어 있습니다", "알림");
+                        }
+                        else
+                        {
+                            SavePwAgentToXml(id, pw, url, filePath);
+                            MessageBox.Show("저장 되었습니다", "알림");
+                        }
+                    }
+                }
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                loadUrlsToListBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message, "오류");
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e) // 아이디 복사버튼
+        {
+            if (!string.IsNullOrEmpty(textBox5.Text))
+            {
+                Clipboard.SetText(textBox5.Text);
+            }
+            else
+            {
+                label7.Visible = true;
+                label7.Text = "먼저 리스트에서 확인할 아이템을 선택해 주세요";
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e) // 비밀번호 복사버튼
+        {
+            if (!string.IsNullOrEmpty(textBox6.Text))
+            {
+                Clipboard.SetText(textBox6.Text);
+            }
+            else
+            {
+                label7.Visible = true;
+                label7.Text = "먼저 리스트에서 확인할 아이템을 선택해 주세요";
+            }
+        }
+       
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            string currentFolderPath = AppDomain.CurrentDomain.BaseDirectory;
+            System.Diagnostics.Process.Start(currentFolderPath);
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedUrl = listBox1.SelectedItem.ToString();
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load("pwAgent.xml");
+
+                XmlNodeList pwAgentlNodes = doc.SelectNodes($"//pwAgent[url='{selectedUrl}']");
+
+                if (pwAgentlNodes.Count > 0)
+                {
+                    XmlNode selectedNode = pwAgentlNodes[0];
+
+                    string id = selectedNode.SelectSingleNode("id").InnerText;
+                    string password = selectedNode.SelectSingleNode("password").InnerText;
+                    string url = selectedNode.SelectSingleNode("url").InnerText;
+
+                    textBox4.Text = url;
+                    textBox5.Text = id;
+                    textBox6.Text = password;
+                }
+                else
+                {
+                    textBox5.Text = "";
+                    textBox6.Text = "";
+                }
+            }
+        }
+
+        private void SavePwAgentToXml(string id, string pw, string url, string filePath)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    doc.Load(filePath);
+                }
+                else
+                {
+                    XmlElement root = doc.CreateElement("pwAgents");
+                    doc.AppendChild(root);
+                }
+
+                XmlElement pwAgent = doc.CreateElement("pwAgent");
+
+                XmlElement idElement = doc.CreateElement("id");
+                idElement.InnerText = id;
+                pwAgent.AppendChild(idElement);
+
+                XmlElement passwordElement = doc.CreateElement("password");
+                passwordElement.InnerText = pw;
+                pwAgent.AppendChild(passwordElement);
+
+                XmlElement urlElement = doc.CreateElement("url");
+                urlElement.InnerText = url;
+                pwAgent.AppendChild(urlElement);
+
+                doc.DocumentElement?.AppendChild(pwAgent);
+
+                doc.Save(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("오류" + ex.Message, "오류");
+            }
+        }
+
+        private void searchURLFromXml(string url, string filePath)
+        {
+            XmlDocument doc = new XmlDocument();
+            if (File.Exists(filePath))
+            {
+                doc.Load(filePath);
+
+                // URL에 대한 자격 증명을 찾습니다.
+                XmlNodeList pwAgents = doc.SelectNodes($"//pwAgent[url='{url}']");
+                if (pwAgents.Count == 1)
+                {
+                    if (InvokeRequired == true)
+                    {
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            foreach (XmlNode pwAgentNode in pwAgents)
+                            {
+                                string id = pwAgentNode.SelectSingleNode("id").InnerText;
+                                string password = pwAgentNode.SelectSingleNode("password").InnerText;
+
+                                textBox5.Text = id;
+                                textBox6.Text = password;
+                            }
+                        }));
+                    }
+                    else
+                    {
+                        foreach (XmlNode pwAgentNode in pwAgents)
+                        {
+                            string id = pwAgentNode.SelectSingleNode("id").InnerText;
+                            string password = pwAgentNode.SelectSingleNode("password").InnerText;
+
+                            textBox5.Text = id;
+                            textBox6.Text = password;
+                        }
+                    }
+                }
+                else
+                {
+                    label7.Visible = true;
+                    label7.Text = "입력한 URL 을 찾을수 없습니다";
+                }
+            }
+            else
+            {
+                label7.Visible = true;
+                label7.Text = "파일을 찾을 수 없습니다.";
+            }
+        }
+
+        private void loadUrlsToListBox()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+
+                XmlNodeList pwAgentlNodes = doc.SelectNodes("//pwAgent/url");
+
+                foreach (XmlNode node in pwAgentlNodes)
+                {
+                    listBox1.Items.Add(node.InnerText);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("아직 표시할 목록이 없습니다. \n관리하실 비밀번호를 등록해 주세요","알림",MessageBoxButtons.OK , MessageBoxIcon.Information);
+            }
+        }
+    }
+}
